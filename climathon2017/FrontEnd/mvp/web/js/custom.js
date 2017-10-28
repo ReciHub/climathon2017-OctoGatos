a4f9ec0fb27df15005c501273e8f3985cddb78b4
Thiago Augusto <martinsthiagoaugusto@gmail.com>
2017-10-28 21:57:22 +0000

    Versão MVP 1

--- climathon2017/FrontEnd/mvp/web/js/custom.js
+++ climathon2017/FrontEnd/mvp/web/js/custom.js
@@ -34,3 +34,4 @@ $(function() {
         element.addClass('active');
     }
 });
+
---
+++ climathon2017/FrontEnd/mvp/web/js/moeda.js
@@ -0,0 +1,6 @@
+var carbobits = 0;
+
+$(document).ready(function() {
+    $("#target1").css("color", "red");
+    
+  });
